3d6708580f9e6564d78542431c3df3db7114eab9
19-mira <105423514+19-mira@users.noreply.github.com>
2022-05-18 18:40:51 +0800

    Update index.jsx
    turn to the document imej

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import React from "react";
+import EditorLayout from "../../layouts/EditorLayout";
+import PrimaryButton from "../../components/Button";
 const SignInComponent = () => {
     return (
         <>
