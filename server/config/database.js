c343b88625d67cae708e723a5db21d06dfa483ab
allison4600 <allison4600@gmail.com>
2022-06-18 03:00:54 +0800

    No change

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
+require("dotenv").config();
+const mysql = require("mysql2");
 
-// const dbconnection = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PW,
-//     database: process.env.DB_NAME,
-// });
+const dbconnection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+});
 
-// module.exports = dbconnection;
+module.exports = dbconnection;
